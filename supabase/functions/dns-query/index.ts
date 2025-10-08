f462eb08a7dfe4dc89525452e405b1197c964b52
Denys Kushnarov <154005444+reginleif888@users.noreply.github.com>
2025-10-08 12:44:07 +0200

    Fix DNS Query

--- supabase/functions/dns-query/index.ts
+++ supabase/functions/dns-query/index.ts
@@ -24,8 +24,7 @@ interface DnsQueryResponse {
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Headers": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
